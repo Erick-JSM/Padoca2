329eca5df91f52635bd22af28d5b7e2a4cd9276d
Erick-JSM <erickjunio187@gmail.com>
2022-10-12 23:44:10 -0300

    Padoca da Esquina

--- app/src/main/java/com/erickjsm/padoca/MenuActivity.java
+++ app/src/main/java/com/erickjsm/padoca/MenuActivity.java
@@ -10,7 +10,6 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import com.erickjsm.padoca.DAO.DAO;
 import com.erickjsm.padoca.adapter.RecycleViewAdapter;
